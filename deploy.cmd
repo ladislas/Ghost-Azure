3d0e0c986e2ddc6b82461ba003616bf42f2660a9
Radoslav Gatev <radoslavgatev@gmail.com>
2018-08-22 14:35:11 +0300

    test deploym,ent

--- deploy.cmd
+++ deploy.cmd
@@ -109,7 +109,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Handle database creation and migrations.
 IF EXIST "%DEPLOYMENT_TARGET%\db.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NODE_EXE! db.js
+  call :ExecuteCmd "!NODE_EXE!" db.js
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
